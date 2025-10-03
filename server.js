a256c46b48788de1dcf2c29656bc592d445f5d8d
LimitlessOI <lifeos@hopkinsgroup.org>
2025-10-03 16:37:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-const express = require('express');
+// Crash visibility
+process.on('uncaughtException', (err) => {
+  console.error('FATAL uncaughtException:', err);
+  process.exit(1);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('FATAL unhandledRejection:', err);
+  process.exit(1);
+});const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 8080;
