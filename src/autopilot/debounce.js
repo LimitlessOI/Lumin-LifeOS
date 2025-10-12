107473169b890c28331c2746a3384b25954950b4
LimitlessOI <lifeos@hopkinsgroup.org>
2025-10-12 11:17:10 -0700

    chore(auto): update src/autopilot/debounce.js

--- src/autopilot/debounce.js
+++ src/autopilot/debounce.js
@@ -1,2 +1,2 @@
 // TODO(auto-modify): Review Debounce Logic
-// hint: Examine and optimize the debounce logic to reduce unnecessary build requests.
+// hint: Examine and potentially refactor the debounce logic to improve handling of rapid build requests.
