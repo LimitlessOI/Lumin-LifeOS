5f1fda261790b9f075f868501ea7b423ce999991
LimitlessOI <lifeos@hopkinsgroup.org>
2025-10-12 04:10:16 -0700

    chore(auto): update src/autopilot/logger.js

--- src/autopilot/logger.js
+++ src/autopilot/logger.js
@@ -1,2 +1,2 @@
 // TODO(auto-modify): Enhance Logging for Build Failures
-// hint: Add detailed logging for build initiation and failure events.
+// hint: Add detailed logging for build failures to capture error messages and stack traces.
