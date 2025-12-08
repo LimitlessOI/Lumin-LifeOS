b1ee23956c67b2d373bd10c115690bb1c4f3544c
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-07 20:47:04 -0800

    Auto-deployment: Updated db/migrations/20231031-create-user-profiles-and-itineraries.js

---
+++ db/migrations/20231031-create-user-profiles-and-itineraries.js
@@ -0,0 +1,61 @@
+```javascript
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user_profiles', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+      },
+      preferences: {
+        type: Sequelize.JSONB,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      }
+    });
+
+    await queryInterface.createTable('itineraries', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      details: {
+        type: Sequelize.JSONB,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('itineraries');
+    await queryInterface.dropTable('user_profiles');
+  }
+};
+```
