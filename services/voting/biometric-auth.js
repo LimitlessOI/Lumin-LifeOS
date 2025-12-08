fdadedb6b6db11e297ea1b2365b333533126c032
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-07 21:06:04 -0800

    Auto-deployment: Updated services/voting/biometric-auth.js

---
+++ services/voting/biometric-auth.js
@@ -0,0 +1,8 @@
+```javascript
+// Placeholder for biometric authentication service
+module.exports = {
+  authenticate: async (userId, deviceInfo) => {
+    // Logic for WebAuthn/CTAP2 communication
+  }
+};
+```
