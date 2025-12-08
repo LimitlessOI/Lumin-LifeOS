22a6e8a9db9b4dc80d618539a2fb6e82deccbc28
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-07 21:06:08 -0800

    Auto-deployment: Updated config/voting-config.js

---
+++ config/voting-config.js
@@ -0,0 +1,8 @@
+```javascript
+// Configuration parameters for the voting protocol
+module.exports = {
+  votingSessionDuration: 3600, // in seconds
+  maxVotesPerSession: 1000,
+  // Add more parameters as needed
+};
+```
