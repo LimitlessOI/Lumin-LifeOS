b8c2536109c37b90c152f3b31c9390bcc2738c7d
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-09 07:27:21 -0800

    Auto-deployment: Updated config/wearable-apis.js

---
+++ config/wearable-apis.js
@@ -0,0 +1,10 @@
+```javascript
+module.exports = {
+  apiKey: 'YOUR_API_KEY_HERE',
+  baseUrl: 'https://api.wearable.com',
+  endpoints: {
+    dataSync: '/data',
+    userMetrics: '/metrics'
+  }
+};
+```
