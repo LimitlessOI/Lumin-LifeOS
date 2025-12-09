93100902a5a801034296f6f21637526ce9b5298f
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-08 16:52:08 -0800

    Auto-deployment: Updated frontend/components/LearningInterface.jsx

---
+++ frontend/components/LearningInterface.jsx
@@ -0,0 +1,13 @@
+```jsx
+import React from 'react';
+
+function LearningInterface({ content }) {
+  return (
+    <div>
+      <h1>Learning Interface</h1>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default LearningInterface;
