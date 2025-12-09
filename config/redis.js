6bc5bb5b011169569cd9e9c5a2b7358ffdd40cc7
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-09 09:55:32 -0800

    Auto-deployment: Updated config/redis.js

---
+++ config/redis.js
@@ -0,0 +1,10 @@
+```javascript
+const Redis = require('ioredis');
+
+const redis = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+});
+
+module.exports = redis;
+```
