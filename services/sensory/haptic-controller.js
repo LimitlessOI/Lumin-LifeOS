dcc41003f7fa5cbea8f239a15a8096aa712c7041
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-09 07:14:00 -0800

    Auto-deployment: Updated services/sensory/haptic-controller.js

---
+++ services/sensory/haptic-controller.js
@@ -0,0 +1,6 @@
+function triggerHapticFeedback(intensity) {
+  // Placeholder for haptic feedback logic
+  console.log(`Triggering haptic feedback with intensity: ${intensity}`);
+}
+
+module.exports = { triggerHapticFeedback };
