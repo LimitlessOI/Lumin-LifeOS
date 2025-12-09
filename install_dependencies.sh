312de50d1ac64008e6c4cf1e26d67ffe957d7aae
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-09 10:05:40 -0800

    Auto-deployment: Updated install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+```bash
+#!/bin/bash
+
+# Navigate to project directory
+cd lifeos-project
+
+# Install dependencies
+npm install tensorflow redis amqplib axios joi winston jest supertest openai pg
+```
