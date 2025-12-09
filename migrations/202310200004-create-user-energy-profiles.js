76d6b7de4e54d52b8a763b7b23c316d344e0d670
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-08 17:37:34 -0800

    Auto-deployment: Updated migrations/202310200004-create-user-energy-profiles.js

---
+++ migrations/202310200004-create-user-energy-profiles.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user_energy_profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        unique: true
+      },
+      energyConsumption: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user_energy_profiles');
+  }
+};
