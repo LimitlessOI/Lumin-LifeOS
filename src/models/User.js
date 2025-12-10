64ea175fc2e0f2c572eaed18546f04d3def159f8
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 00:48:16 -0800

    Auto-deployment: Updated src/models/User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,36 @@
-```javascript
-const { Pool } = require('pg');
-const pool = new Pool(); // Configure your connection details in .env
+const pool = require('../utils/database');
 
-const createUser = async (username, email, passwordHash) => {
-  const result = await pool.query(
-    'INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *',
-    [username, email, passwordHash]
-  );
-  return result.rows[0];
-};
+class User {
+  static async getAll() {
+    const res = await pool.query('SELECT * FROM users');
+    return res.rows;
+  }
 
-const findUserByUsername = async (username) => {
-  const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-  return result.rows[0];
-};
+  static async create({ name, email }) {
+    const res = await pool.query(
+      'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+      [name, email]
+    );
+    return res.rows[0];
+  }
 
-module.exports = { createUser, findUserByUsername };
-```
+  static async getById(id) {
+    const res = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    return res.rows[0];
+  }
+
+  static async update(id, { name, email }) {
+    const res = await pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *',
+      [name, email, id]
+    );
+    return res.rows[0];
+  }
+
+  static async delete(id) {
+    const res = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+    return res.rowCount > 0;
+  }
+}
+
+module.exports = User;
