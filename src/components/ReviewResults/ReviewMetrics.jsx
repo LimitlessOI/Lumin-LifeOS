6a9b2f43568a28ebc52af71032932e868aa9d40d
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 06:55:13 -0800

    Auto-deployment: Updated src/components/ReviewResults/ReviewMetrics.jsx

---
+++ src/components/ReviewResults/ReviewMetrics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReviewMetrics = ({ reviews }) => {
+    return (
+        <div>
+            <h2>Review Metrics</h2>
+            {/* Implement metrics display using metrics data */}
+        </div>
+    );
+};
+
+export default ReviewMetrics;
