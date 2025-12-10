8dbc3b45ff625b89abf11541a43b64dfe6c6c415
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 02:19:49 -0800

    Auto-deployment: Updated src/components/landing/AIContentLanding.jsx

---
+++ src/components/landing/AIContentLanding.jsx
@@ -0,0 +1,18 @@
+```jsx
+import React from 'react';
+import { HeroSection, ServiceOverview, BenefitsList, PricingSection, CTASection } from './';
+
+const AIContentLanding = () => {
+  return (
+    <div className="landing-page">
+      <HeroSection />
+      <ServiceOverview />
+      <BenefitsList />
+      <PricingSection />
+      <CTASection />
+    </div>
+  );
+};
+
+export default AIContentLanding;
+```
