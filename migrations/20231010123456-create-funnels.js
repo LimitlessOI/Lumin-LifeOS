7ac5164c5aa2dd70ec058ff4fca26b8404d56f60
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 01:32:23 -0800

    Auto-deployment: Updated migrations/20231010123456-create-funnels.js

---
+++ migrations/20231010123456-create-funnels.js
@@ -0,0 +1,38 @@
+```javascript
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Funnels', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Funnels');
+  }
+};
+```
