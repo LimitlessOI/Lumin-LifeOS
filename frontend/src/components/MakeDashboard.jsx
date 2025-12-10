484115c837364329bc8d2cad4347d1bd692c3f56
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 00:42:57 -0800

    Auto-deployment: Updated frontend/src/components/MakeDashboard.jsx

---
+++ frontend/src/components/MakeDashboard.jsx
@@ -0,0 +1,14 @@
+```jsx
+import React from 'react';
+import ScenarioList from './ScenarioList';
+
+const MakeDashboard = () => {
+  return (
+    <div>
+      <h1>Make Dashboard</h1>
+      <ScenarioList />
+    </div>
+  );
+};
+
+export default MakeDashboard;
