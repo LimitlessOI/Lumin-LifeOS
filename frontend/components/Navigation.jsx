80fdc04776008f80e6d0fcd78a8414a5aed79f64
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 08:17:22 -0800

    Auto-deployment: Updated frontend/components/Navigation.jsx

---
+++ frontend/components/Navigation.jsx
@@ -0,0 +1,18 @@
+```javascript
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                {/* Add more links as needed */}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
+```
