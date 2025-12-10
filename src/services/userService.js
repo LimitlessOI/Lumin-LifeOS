31512e35605a0adf1ff7875dfc12ef6d3a23c20c
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 05:16:59 -0800

    Auto-deployment: Updated src/services/userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,24 +1,21 @@
 ```javascript
-const UserRepository = require('../repositories/userRepository');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { v4: uuidv4 } = require('uuid');
+const User = require('../models/User');
 
-class UserService {
-    async registerUser(username, email, password) {
-        const passwordHash = await bcrypt.hash(password, 10);
-        return UserRepository.createUser(username, email, passwordHash);
-    }
+exports.createUser = async (username, password) => {
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+  return await User.create({ username, password_hash: passwordHash });
+};
 
-    async authenticateUser(email, password) {
-        const user = await UserRepository.getUserByEmail(email);
-        if (user && await bcrypt.compare(password, user.password_hash)) {
-            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            return { user, token };
-        }
-        throw new Error('Authentication failed');
-    }
-}
+exports.authenticateUser = async (username, password) => {
+  const user = await User.findOne({ where: { username } });
+  if (!user) throw new Error('User not found');
 
-module.exports = new UserService();
+  const validPassword = await bcrypt.compare(password, user.password_hash);
+  if (!validPassword) throw new Error('Invalid password');
+
+  return jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
 ```
