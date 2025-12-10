e78f8198e7ad5c589e64087e01f728ad92bc24fd
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 08:17:20 -0800

    Auto-deployment: Updated frontend/components/LoadingSpinner.jsx

---
+++ frontend/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+```javascript
+import React from 'react';
+import './LoadingSpinner.css';
+
+function LoadingSpinner() {
+    return <div className="loading-spinner">Loading...</div>;
+}
+
+export default LoadingSpinner;
+```
