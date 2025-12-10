2e6a81b7956adaef07980ab81c8afab0435d7401
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-09 23:51:24 -0800

    Auto-deployment: Updated contracts/ReputationToken.sol

---
+++ contracts/ReputationToken.sol
@@ -0,0 +1,16 @@
+```solidity
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ReputationToken is ERC20 {
+    constructor() ERC20("ReputationToken", "RPT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
+```
