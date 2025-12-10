0883c27b057bda5e8e691aa467b4586e0a61d457
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 03:46:15 -0800

    Auto-deployment: Updated frontend/components/IntegrationManager/IntegrationManager.jsx

---
+++ frontend/components/IntegrationManager/IntegrationManager.jsx
@@ -0,0 +1,14 @@
+```jsx
+import React from 'react';
+import IntegrationList from './IntegrationList';
+
+const IntegrationManager = () => {
+    return (
+        <div>
+            <h1>Integration Manager</h1>
+            <IntegrationList />
+        </div>
+    );
+};
+
+export default IntegrationManager;
