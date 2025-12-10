501ef19ae2485cba39387d191a417a43ae26fc6a
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-09 21:56:23 -0800

    Auto-deployment: Updated frontend/src/pages/CodeGenerator.jsx

---
+++ frontend/src/pages/CodeGenerator.jsx
@@ -0,0 +1,15 @@
+```jsx
+import React from 'react';
+import CodeEditor from '../components/CodeEditor';
+
+const CodeGenerator = () => {
+  return (
+    <div>
+      <h1>Code Generation</h1>
+      <CodeEditor />
+    </div>
+  );
+};
+
+export default CodeGenerator;
+```
