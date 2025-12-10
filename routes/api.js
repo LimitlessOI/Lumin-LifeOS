778cd582be82e3292909f3cdd70dc3712e12bf15
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 05:14:23 -0800

    Auto-deployment: Updated routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const ConfigService = require('../services/ConfigService');
 const router = express.Router();
-const callAnalyzerController = require('../controllers/callAnalyzerController');
 
-router.post('/analyze-call', callAnalyzerController.analyzeCall);
+router.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+router.get('/config', (req, res) => {
+  try {
+    const configKeys = ['NODE_ENV', 'PORT', 'DB_HOST', 'DB_NAME'];
+    const config = configKeys.reduce((acc, key) => {
+      acc[key] = ConfigService.get(key);
+      return acc;
+    }, {});
+    res.json(config);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 module.exports = router;
