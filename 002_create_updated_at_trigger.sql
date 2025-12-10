c821757a5be7d869b6e5beada71a88f10c04ced6
LimitlessOI <lifeos@hopkinsgroup.org>
2025-12-10 08:37:10 -0800

    Auto-deployment: Updated 002_create_updated_at_trigger.sql

---
+++ 002_create_updated_at_trigger.sql
@@ -0,0 +1,14 @@
+```sql
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_user_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+```
