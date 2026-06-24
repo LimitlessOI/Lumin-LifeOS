7afb3ac7d65f3719daaac16d66cd76977da7a299
LimitlessOI <lifeos@hopkinsgroup.org>
2026-06-24 13:44:59 -0700

    [system-build] rewrite entire server.js from scratch with new architecture

--- server.js
+++ server.js
@@ -6,11 +6,38 @@ import config from './src/config.js';
 import logger from './src/utils/logger.js';
 
 const PORT = config.port;
+let server;
 
 const startServer = () => {
-  app.listen(PORT, () => {
+  server = app.listen(PORT, () => {
     logger.info(`LifeOS server running in ${config.env} mode on port ${PORT}`);
   });
 };
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
 
 startServer();
