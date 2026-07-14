ccb5b3b57c7f5f9360e2da0bea97632a25a42095
LimitlessOI <lifeos@hopkinsgroup.org>
2026-07-13 18:10:09 -0700

    GOVERNED-AUTONOMOUS-SHIP: memory-system 7

--- db/migrations/202311_capsule_id_format.sql
+++ db/migrations/202311_capsule_id_format.sql
@@ -2,19 +2,18 @@
 ALTER TABLE capsules
   ALTER COLUMN id SET DEFAULT gen_random_uuid();
 
-ALTER TABLE capsules
-  ALTER COLUMN id SET DATA TYPE uuid
-  USING id::uuid;
-
 DO $$
 BEGIN
   IF NOT EXISTS (
     SELECT 1
     FROM pg_constraint
-    WHERE conname = 'capsules_id_check'
+    WHERE conname = 'capsules_id_uuid_v4_check'
   ) THEN
     ALTER TABLE capsules
-      ADD CONSTRAINT capsules_id_check
-      CHECK (id IS NOT NULL);
+      ADD CONSTRAINT capsules_id_uuid_v4_check
+      CHECK (
+        id IS NULL
+        OR id::text ~ '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+      ) NOT VALID;
   END IF;
 END $$;
