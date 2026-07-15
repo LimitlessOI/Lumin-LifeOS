a7d1c57f2d5c46207e5252b5be76d43f36df626a
LimitlessOI <lifeos@hopkinsgroup.org>
2026-07-15 10:30:32 -0700

    GOVERNED-AUTONOMOUS-SHIP: business-tools step10

---
+++ services/extractRoutes.js
@@ -0,0 +1,20 @@
+/**
+ * SYNOPSIS: Existing code in services/extractRoutes.js
+ */
+// Existing code in services/extractRoutes.js
+// This file handles the extraction of sub-features from server.js
+
+export function extractSubRoutes(server) {
+    const subRoutes = [];
+    
+    server.routes.forEach(route => {
+        if (route.isSubFeature) {
+            subRoutes.push(route);
+        }
+    });
+
+    return subRoutes;
+}
+
+// Additional code can be added below if needed, preserving existing functionality
+
