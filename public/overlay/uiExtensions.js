417da09ddcb5c086ff458ccdf78e19e1a6183273
LimitlessOI <lifeos@hopkinsgroup.org>
2026-07-16 11:10:23 -0700

    GOVERNED-AUTONOMOUS-SHIP: universal-overlay universal-overlay-implement-extension-icons

--- public/overlay/uiExtensions.js
+++ public/overlay/uiExtensions.js
@@ -1,10 +1,10 @@
 /**
- * SYNOPSIS: LifeOS overlay UI — UiExtensions.
+ * SYNOPSIS: Exports new — public/overlay/uiExtensions.js.
  * @ssot docs/products/universal-overlay/PRODUCT_HOME.md
  */
 [
   {
-    "old_string": "export const extensionIcons = {\n  '16px': 'path/to/lifeos-icon-16.png',\n  '32px': 'path/to/lifeos-icon-32.png',\n  '48px': 'path/to/lifeos-icon-48.png',\n  '128px': 'path/to/lifeos-icon-128.png'\n};",
-    "new_string": "export const extensionIcons = {\n  '16px': 'https://s3.amazonaws.com/lifeos/extension-icon-16.png',\n  '32px': 'https://s3.amazonaws.com/lifeos/extension-icon-32.png',\n  '48px': 'https://s3.amazonaws.com/lifeos/extension-icon-48.png',\n  '128px': 'https://s3.amazonaws.com/lifeos/extension-icon-128.png'\n};"
+    "old_string": "   export const extensionIcons = {\n     '16px': 'https://s3.amazonaws.com/lifeos/extension-icon-16.png',\n     '32px': 'https://s3.amazonaws.com/lifeos/extension-icon-32.png',\n     '48px': 'https://s3.amazonaws.com/lifeos/extension-icon-48.png',\n     '128px': 'https://s3.amazonaws.com/lifeos/extension-icon-128.png'\n   };",
+    "new_string": "   export const extensionIcons = {\n     '16px': 'https://s3.amazonaws.com/lifeos/extension-icon-16.png',\n     '32px': 'https://s3.amazonaws.com/lifeos/extension-icon-32.png',\n     '48px': 'https://s3.amazonaws.com/lifeos/extension-icon-48.png',\n     '128px': 'https://s3.amazonaws.com/lifeos/extension-icon-128.png'\n   };\n\n   export function new IconComponent(size) {\n     return `<img src=\"${extensionIcons[size]}\" alt=\"LifeOS Icon\" />`;\n   }"
   }
 ]
