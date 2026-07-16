67b17056ae08786422bd2280c94cc55f5ae5db91
LimitlessOI <lifeos@hopkinsgroup.org>
2026-07-16 01:58:54 -0700

    GOVERNED-AUTONOMOUS-SHIP: lumin-university lumin-university-2

--- db/migrations/007_update_mentor_criteria.sql
+++ db/migrations/007_update_mentor_criteria.sql
@@ -3,14 +3,14 @@
 
 DO $$
 BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM information_schema.tables
-        WHERE table_name = 'mentor_criteria'
-    ) THEN
-        ALTER TABLE mentor_criteria
-            ADD COLUMN IF NOT EXISTS evaluator_certification VARCHAR(255),
-            ADD COLUMN IF NOT EXISTS evaluator_experience_years INT,
-            ADD COLUMN IF NOT EXISTS evaluator_approved BOOLEAN DEFAULT FALSE;
-    END IF;
+    -- Create table if it does not exist
+    CREATE TABLE IF NOT EXISTS mentor_criteria (
+        id SERIAL PRIMARY KEY -- Assuming an id column for uniqueness
+    );
+
+    -- Add new columns if they do not exist
+    ALTER TABLE mentor_criteria
+        ADD COLUMN IF NOT EXISTS evaluator_certification VARCHAR(255),
+        ADD COLUMN IF NOT EXISTS evaluator_experience_years INT,
+        ADD COLUMN IF NOT EXISTS evaluator_approved BOOLEAN DEFAULT FALSE;
 END $$;
