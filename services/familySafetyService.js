d284d8e50f420078e9e779ef8b8af90b500fec98
LimitlessOI <lifeos@hopkinsgroup.org>
2026-07-31 00:01:16 -0700

    GOVERNED-AUTONOMOUS-SHIP: faith-studio 02

--- services/familySafetyService.js
+++ services/familySafetyService.js
@@ -11,21 +11,25 @@ export function applyFamilySafetyFilters(content, safetyMode = 'family') {
   switch (safetyMode) {
     case 'church':
       // Stricter filtering for church mode
-      filteredContent = filteredContent.replace(/sex|porn|nude|erotic/gi, '[redacted]');
-      filteredContent = filteredContent.replace(/fuck|shit|bitch|asshole|cunt/gi, '[censored]');
-      filteredContent = filteredContent.replace(/alcohol|beer|wine|liquor/gi, '[beverage]');
-      filteredContent = filteredContent.replace(/drugs|weed|cocaine|heroin/gi, '[substance]');
+      filteredContent = filteredContent.replace(/sex|porn|nude|erotic|masturbate|orgasm|intercourse|penis|vagina|clitoris|anal|oral/gi, '[redacted]');
+      filteredContent = filteredContent.replace(/fuck|shit|bitch|asshole|cunt|bastard|damn|hell|piss|tits|dick/gi, '[censored]');
+      filteredContent = filteredContent.replace(/alcohol|beer|wine|liquor|whiskey|vodka|gin|rum/gi, '[beverage]');
+      filteredContent = filteredContent.replace(/drugs|weed|cocaine|heroin|meth|opioid|psychedelic/gi, '[substance]');
+      filteredContent = filteredContent.replace(/violence|kill|murder|slaughter|torture|rape|assault/gi, '[harmful act]');
       break;
     case 'classroom':
       // Moderate filtering for classroom mode
-      filteredContent = filteredContent.replace(/fuck|shit|bitch|asshole/gi, '[censored]');
-      filteredContent = filteredContent.replace(/sex|porn/gi, '[sensitive topic]');
+      filteredContent = filteredContent.replace(/fuck|shit|bitch|asshole|cunt|bastard|damn/gi, '[censored]');
+      filteredContent = filteredContent.replace(/sex|porn|nude|erotic|masturbate/gi, '[sensitive topic]');
+      filteredContent = filteredContent.replace(/alcohol|drugs/gi, '[restricted content]');
+      filteredContent = filteredContent.replace(/kill|murder|rape/gi, '[harmful act]');
       break;
     case 'family':
     default:
       // Default family mode filtering
-      filteredContent = filteredContent.replace(/fuck|shit/gi, '[censored]');
-      filteredContent = filteredContent.replace(/porn/gi, '[sensitive]');
+      filteredContent = filteredContent.replace(/fuck|shit|bitch/gi, '[censored]');
+      filteredContent = filteredContent.replace(/porn|nude/gi, '[sensitive]');
+      filteredContent = filteredContent.replace(/drugs/gi, '[substance]');
       break;
   }
 
