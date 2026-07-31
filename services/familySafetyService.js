76bf8285877bf6b3a42224efc3f65ae9f87d7b12
LimitlessOI <lifeos@hopkinsgroup.org>
2026-07-31 00:58:25 -0700

    GOVERNED-AUTONOMOUS-SHIP: faith-studio 02

--- services/familySafetyService.js
+++ services/familySafetyService.js
@@ -16,6 +16,9 @@ export function applyFamilySafetyFilters(content, safetyMode = 'family') {
       filteredContent = filteredContent.replace(/alcohol|beer|wine|liquor|whiskey|vodka|gin|rum/gi, '[beverage]');
       filteredContent = filteredContent.replace(/drugs|weed|cocaine|heroin|meth|opioid|psychedelic/gi, '[substance]');
       filteredContent = filteredContent.replace(/violence|kill|murder|slaughter|torture|rape|assault/gi, '[harmful act]');
+      // Additional terms for church mode
+      filteredContent = filteredContent.replace(/blasphemy|heresy|apostasy|sacrilege/gi, '[theological term]');
+      filteredContent = filteredContent.replace(/cult|sect|pagan|witchcraft|occult/gi, '[religious term]');
       break;
     case 'classroom':
       // Moderate filtering for classroom mode
@@ -23,6 +26,9 @@ export function applyFamilySafetyFilters(content, safetyMode = 'family') {
       filteredContent = filteredContent.replace(/sex|porn|nude|erotic|masturbate/gi, '[sensitive topic]');
       filteredContent = filteredContent.replace(/alcohol|drugs/gi, '[restricted content]');
       filteredContent = filteredContent.replace(/kill|murder|rape/gi, '[harmful act]');
+      // Additional terms for classroom mode
+      filteredContent = filteredContent.replace(/suicide|self-harm/gi, '[sensitive topic]');
+      filteredContent = filteredContent.replace(/gambling|betting/gi, '[restricted activity]');
       break;
     case 'family':
     default:
@@ -30,6 +36,9 @@ export function applyFamilySafetyFilters(content, safetyMode = 'family') {
       filteredContent = filteredContent.replace(/fuck|shit|bitch/gi, '[censored]');
       filteredContent = filteredContent.replace(/porn|nude/gi, '[sensitive]');
       filteredContent = filteredContent.replace(/drugs/gi, '[substance]');
+      // Additional terms for family mode
+      filteredContent = filteredContent.replace(/violence|gore/gi, '[graphic content]');
+      filteredContent = filteredContent.replace(/profanity|cursing/gi, '[inappropriate language]');
       break;
   }
 
